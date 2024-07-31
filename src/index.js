afdf64c6bdef3fc5613a4f3f6cbe47cf6a37d8d6
+ <+>
2024-07-30 21:00:31 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response/JSON())
+        .then((data) => {
+            const title = document.querySelector("selction#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
